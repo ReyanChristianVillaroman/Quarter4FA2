8815a83ed7355a6f92995f1d3b9b0aba76c8c67e
ReyanChristianVillaroman <reyanchristian.villaroman@smc.pshs.edu.ph>
2025-04-30 21:39:39 +0800

    Add files via upload

---
+++ FA2VILLAROMAN.js
@@ -0,0 +1,27 @@
+function generateTable() {
+    const start = parseInt(document.getElementById("startNum").value);
+    const end = parseInt(document.getElementById("endNum").value);
+  
+    if (start < 2 || end > 10 || start > end) {
+      alert("Please enter numbers between 2 and 10");
+      document.getElementById("output").innerHTML = ""; // Clear table
+      return;
+    }
+  
+    let output = "<table><tr><th>x</th>";
+    for (let i = start; i <= end; i++) {
+      output += "<th>" + i + "</th>";
+    }
+    output += "</tr>";
+  
+    for (let i = start; i <= end; i++) {
+      output += "<tr><th>" + i + "</th>";
+      for (let j = start; j <= end; j++) {
+        output += "<td>" + (i * j) + "</td>";
+      }
+      output += "</tr>";
+    }
+  
+    output += "</table>";
+    document.getElementById("output").innerHTML = output;
+  }
